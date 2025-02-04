65d568ef1b5e01188078fe79287251c4b0d58477
Arlesson-Sales <urameshy11@gmail.com>
2025-02-04 00:22:49 -0300

    Gerenciamento de simbolos, numeros e letras na senha gerada

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,13 +1,26 @@
 const interface = {
+    out: document.querySelector("#password-out"),
     copy_buttons: document.querySelectorAll(".copy-button"),
     length_input: document.querySelector("#password-length"),
-    out: document.querySelector("#password-out")
+
+    //Configurações da senha gerada.
+    password_config: {
+        labels: document.querySelectorAll("input[type='checkbox']"),
+        "uppercase-checkbox": true,
+        "numbers-checkbox": true,
+        "symbols-checkbox": true
+    }
 }
 
 function generatorPassword(length)
 {
-    const chars = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ1234567890!@#$%&*()[]";
+    let chars = "abcdefghijklmnopqrstuvxwyz";
     let password = "";
+
+    if (interface.password_config["uppercase-checkbox"]) chars += "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+    if (interface.password_config["symbols-checkbox"]) chars += "!@#$%&*()[]";
+    if (interface.password_config["numbers-checkbox"]) chars += "1234567890";
+
     
     for (let index = 0; index < length; index++)
     {
@@ -17,11 +30,27 @@ function generatorPassword(length)
     return password;
 }
 
-//Aplicação de eventos.
-interface.length_input.addEventListener("input", event => {
-    interface.out.value = generatorPassword(event.target.value);
-});
+//Funções para eventos.
+function generateAndShowPassword()
+{
+    let password_length = interface.length_input.value;
+    document.querySelector("#password-controls-container").children[0].innerHTML = `Tamanho <span>${password_length}</span>`;
+    interface.out.value = generatorPassword(password_length);
+}
 
-interface.copy_buttons.forEach(button => {
-    button.addEventListener("click", () => window.navigator.clipboard.writeText(interface.out.value))
-})
+function checkmarkToggle(check_name, index)
+{
+    const checkmark = document.querySelector(".checkbox-controls-container").children[index].firstElementChild;
+    interface.password_config[`${check_name}`] = !interface.password_config[`${check_name}`];
+    checkmark.classList.toggle("checkbox-mark-checked");
+    generateAndShowPassword();
+}
+
+//Aplicação de eventos.
+window.addEventListener("load", generateAndShowPassword);
+//Evento - Gerar uma nova senha a cada interação do usuario com o ranger.
+interface.length_input.addEventListener("input", generateAndShowPassword);
+//Evento - Copiar a senha gerada para a area de transferencia.
+interface.copy_buttons.forEach(button => button.addEventListener("click", () => window.navigator.clipboard.writeText(interface.out.value)));
+//Evento - Definir os caracteres que terá na senha gerada.
+interface.password_config.labels.forEach((label, index) => label.addEventListener("click", (event) => checkmarkToggle(event.target.id, index)));
